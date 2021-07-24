8ecab318805e36da57fb49371372ddd2a93373d4
paulo77dias <paulo.tavares193@gmail.com>
2021-07-24 09:56:26 -0300

    last commit

--- avancoDesafio/src/app/app.component.ts
+++ avancoDesafio/src/app/app.component.ts
@@ -1,10 +1,18 @@
+import { AuthService } from './common/auth/services/authService';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css','./app.component.scss']
 })
 export class AppComponent {
- 
+
+        constructor(private token:AuthService){
+
+        }
+        
+        public logout(){
+                this.token.logout();
+        }
 }
